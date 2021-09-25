d71609129481344e46636a85bb7ef348d8dc937b
Anthony Fadly <anthony.fadly.990@my.csun.edu>
2021-09-25 16:11:20 -0700

    example and start script

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# A Better As-Is Parser
+
+import argparser
+
+verbose = False
+
+
+def main():
+    global verbose
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
