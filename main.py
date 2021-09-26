513c3cdf1a3fdc6aae9de3654967b4f575e844bd
Anthony Fadly <anthony.fadly.990@my.csun.edu>
2021-09-25 18:03:51 -0700

    validate statusline

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 import argparse
 
 verbose = False
+http_codes = [100,101,102,103,200,201,202,203,204,205,206,207,208,226,300,301,302,303,304,305,306,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,421,422,423,424,425,426,428,429,431,451,500,501,502,503,504,505,506,507,508,510,511]
 
 
 def read_file_to_array(filename):
@@ -20,7 +21,29 @@ def read_file_to_array(filename):
 def get_status(line):
     result = []
     statusline = line.split(' ')
-    statusnum = result.append(statusline[1])
+    if len(statusline) != 3:
+        print("Syntax Error on line 1:")
+        print(line)
+        print("Expected 3 terms")
+        exit()
+    if not statusline[0] == 'status:':
+        print("Syntax Error on line 1:")
+        print(line)
+        print("Expected 'status:'")
+        exit()
+    try:
+        statusnum = int(statusline[1])
+    except ValueError:
+        print("Syntax Error on line 1:")
+        print(line)
+        print("Expected an http status code")
+        exit()
+    if not statusnum in http_codes:
+        print("Syntax Error on line 1:")
+        print(line)
+        print(str(statusnum) + " is not a valid http status code")
+        exit()
+    result.append(statusnum)
     result.append(statusline[2])
     return result
 
